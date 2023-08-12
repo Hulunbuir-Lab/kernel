52e8df504a2ff6af9da73602ec6f115caf1873e1
vim233 <2897346274@qq.com>
2023-08-11 19:47:54 -0700

    getcwd

--- lwp/file.cpp
+++ lwp/file.cpp
@@ -1,5 +1,5 @@
 #include <lwp.h>
-
+#include <string.h>
 u64 FileTable::Open(const char *filePath) {
     open(filePath, &sdFile[++processFd]);
     return processFd;
@@ -21,4 +21,9 @@ int FileTable::Write(u64 fd, u8* buf, u64 size) {
     if (sdFile[fd].size != 0) {
         return write(&sdFile[fd], buf, size);
     } else return -1;
+}
+
+int FileTable::Getcwd(u8* buf, u64 size) {
+    memcpy(buf,processController.CurrentProcess->pwd,128);
+    return 10;
 }
